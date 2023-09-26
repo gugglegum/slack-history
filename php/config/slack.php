72cb72a84de8775d3e8ab8fb71372681bb65d5b4
Paul Melekhov <gugglegum@gmail.com>
2023-09-27 03:14:57 +0400

    Added config option SLACK_SKIP_FILES_CHANNELS

--- php/config/slack.php
+++ php/config/slack.php
@@ -3,4 +3,5 @@
 return [
     'token' => getenv('SLACK_TOKEN'),
     'skipHistoryChannels' => getenv('SLACK_SKIP_HISTORY_CHANNELS'),
+    'skipFilesChannels' => getenv('SLACK_SKIP_FILES_CHANNELS'),
 ];
