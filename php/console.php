a87d02003bd24124710ebea56cab87f8cf5ef96d
Paul Melekhov <gugglegum@gmail.com>
2023-09-27 03:11:07 +0400

    Reworked console.php to explicit usage of action classes to help PhpStorm IDE to do the code analysis (to suppress class not used inspection)

--- php/console.php
+++ php/console.php
@@ -5,16 +5,22 @@ require_once __DIR__ . '/init.php';
 
 (function() {
     $resources = new \App\ResourceManager();
-    $actionClass = 'App\\Console\\Actions\\'
-        . implode('', array_map(function($w) { return ucfirst($w); }, explode('-', $_SERVER['argv'][1])))
-        . 'Action';
+//    $actionClass = 'App\\Console\\Actions\\'
+//        . implode('', array_map(function($w) { return ucfirst($w); }, explode('-', $_SERVER['argv'][1])))
+//        . 'Action';
+    $actionClass = match ($_SERVER['argv'][1]) {
+        'fetch-history' => \App\Console\Actions\FetchHistoryAction::class,
+        'fetch-files' => \App\Console\Actions\FetchFilesAction::class,
+        'compile-html' => \App\Console\Actions\CompileHtmlAction::class,
+        'test' => \App\Console\Actions\TestAction::class,
+    };
     $action = new $actionClass($resources);
     try {
-        $statusCode = $action();
+        $statusCode = (int) $action();
     } catch (Throwable $e) {
-        echo "Error: {$e->getMessage()}\n";
-        echo "Code: {$e->getCode()}\n";
         echo "\nException " . get_class($e) . " was thrown in file {$e->getFile()} at line {$e->getLine()}\nDebug backtrace:\n{$e->getTraceAsString()}\n";
+        echo "Code: {$e->getCode()}\n";
+        echo "Message: {$e->getMessage()}\n";
 
         if ($e->getCode() != 0) {
             $statusCode = $e->getCode();
