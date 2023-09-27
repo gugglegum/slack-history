9d378dd82a74328b023f9573fd91436d5fc2eca8
Paul Melekhov <gugglegum@gmail.com>
2023-09-27 05:03:55 +0400

    Removed redundant echo off

--- update.cmd
+++ update.cmd
@@ -1,6 +1,5 @@
 @echo off
 "%~dp0bin\php.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "fetch-history"
-@echo off
 "%~dp0bin\php.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "fetch-files"
 @pause
 "%~dp0bin\php.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "compile-html"
