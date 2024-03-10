21ac3c71d2a08d9facda71653e65952d864d65a4
Paul Melekhov <gugglegum@gmail.com>
2024-03-11 00:49:35 +0400

    Moved pause at the end

--- update.cmd
+++ update.cmd
@@ -1,5 +1,5 @@
 @echo off
 "%~dp0bin\php.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "fetch-history"
 "%~dp0bin\php.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "fetch-files"
-@pause
 "%~dp0bin\php.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "compile-html"
+@pause
