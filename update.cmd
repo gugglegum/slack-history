246c0b1129a484bc5d98e8c3d488f5927cd53469
Paul Melekhov <gugglegum@gmail.com>
2024-03-11 03:50:32 +0400

    Little improvement in update.cmd

--- update.cmd
+++ update.cmd
@@ -1,6 +1,12 @@
 @echo off
-@cd %~dp0
+cd %~dp0
+
 .\bin\php.exe -c .\bin\php.ini -n .\php\console.php fetch-history
+
+echo:
 .\bin\php.exe -c .\bin\php.ini -n .\php\console.php fetch-files
+
+echo:
 .\bin\php.exe -c .\bin\php.ini -n .\php\console.php compile-html
-@pause
+
+pause
